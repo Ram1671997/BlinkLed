38ac8f9c584dd4e5e87078ea171ac5b636939336
Ram1671997 <33631048+Ram1671997@users.noreply.github.com>
2017-11-13 18:21:14 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,19 @@
+/*
+ */
+#include <avr/io.h>
+#include <util/delay.h>
+int main(void)
+{
+
+   DDRC=0xFF;
+
+    while(1){
+     PORTC=0xFF;
+     _delay_ms(250);
+     PORTC=0x00;
+     _delay_ms(250);
+    }
+
+
+
+}
